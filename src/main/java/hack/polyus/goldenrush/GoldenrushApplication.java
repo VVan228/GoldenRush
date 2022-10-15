0610df230c696ba83ce8ecf4191e63672866b644
Leongard <vovchick2210@gmail.com>
2022-10-15 09:19:32 +0800

    init commit

---
+++ src/main/java/hack/polyus/goldenrush/GoldenrushApplication.java
@@ -0,0 +1,13 @@
+package hack.polyus.goldenrush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenrushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenrushApplication.class, args);
+	}
+
+}
